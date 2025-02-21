353fee994e0bda4de846641c7b62ebb2e2766048
sandunx <sandunbalasooriya10@gmail.com>
2025-02-21 00:25:32 +0000

    init

---
+++ DemoApp/DemoApp/src/main/java/com/telusko/DemoApp/UserController.java
@@ -0,0 +1,14 @@
+ package com.telusko.DemoApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping 
+    public String demo(){
+         return "Hello World";
+    }
+
+    
+}
